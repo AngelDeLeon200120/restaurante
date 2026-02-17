e82118289e6cc515a27f229a30ce995b7f275e00
AngelCode2001 <angel.deleon@code.com.gt>
2026-02-16 22:51:27 -0600

    seguir

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,5 @@
+import comida from './imagenes.js';
+
 const carouselImagenes = document.querySelector('.carousel-imagenes');
 
 const img= document.createElement('img');
@@ -5,4 +7,12 @@ img.classList.add('imgActive');
 img.src='./img/lugar2.jpg';
 img.alt = 'carousel lugares';
 carouselImagenes.append(img);
-console.log(carouselImagenes);
+console.log(comida.comida.almuerzos[0].img);
+
+
+const carusel= document.querySelector('.carousel');
+
+carusel.addEventListener('click',function (e) {
+    console.log(e.target);
+    
+})
--- js/imagenes.js
+++ js/imagenes.js
@@ -5,31 +5,31 @@ export default {
         id: 1,
         descripcion: 'Huevos revueltos con jamón, acompañados de frijoles volteados, pan tostado y rodajas de aguacate.',
         precio: 35,
-        img: '../img/desayuno1.png'
+        img: './img/desayuno1.png'
       },
       {
         id: 2,
         descripcion: 'Omelette de queso y vegetales (cebolla, tomate y chile dulce), servido con pan artesanal y café.',
         precio: 40,
-        img: '../img/desayuno2.png'
+        img: './img/desayuno2.png'
       },
       {
         id: 3,
         descripcion: 'Pan francés dorado en mantequilla, acompañado de miel, fruta fresca y un toque de azúcar glass.',
         precio: 25,
-        img: '../img/desayuno3.png'
+        img: './img/desayuno3.png'
       },
       {
         id: 4,
         descripcion: 'Avena caliente preparada con leche, canela, rodajas de banano y un toque de miel natural.',
         precio: 20,
-        img: '../img/desayuno4.png'
+        img: './img/desayuno4.png'
       },
       {
         id: 5,
         descripcion: 'Desayuno típico con huevos estrellados, frijoles, plátano frito, queso fresco y tortillas.',
         precio: 37,
-        img: '../img/desayuno5.png'
+        img: './img/desayuno5.png'
       }
     ],
     almuerzos: [
@@ -37,31 +37,31 @@ export default {
         id: 6,
         descripcion: 'Pechuga de pollo a la plancha, arroz blanco, ensalada fresca de lechuga y tomate, y frijoles.',
         precio: 45,
-        img: '../img/almuerzo1.png'
+        img: './img/almuerzo1.png'
       },
       {
         id: 7,
         descripcion: 'Carne asada jugosa, acompañada de papas fritas, ensalada verde y tortillas recién hechas.',
         precio: 50,
-        img: '../img/almuerzo2.png'
+        img: './img/almuerzo2.png'
       },
       {
         id: 8,
         descripcion: 'Espagueti en salsa de tomate casera, con queso rallado y pan de ajo.',
         precio: 35,
-        img: '../img/almuerzo3.png'
+        img: './img/almuerzo3.png'
       },
       {
         id: 9,
         descripcion: 'Arroz frito con pollo, huevo, vegetales salteados y salsa de soya.',
         precio: 30,
-        img: '../img/almuerzo4.png'
+        img: './img/almuerzo4.png'
       },
       {
         id: 10,
         descripcion: 'Filete de pescado empanizado, servido con arroz, ensalada de repollo y limón.',
         precio: 47,
-        img: '../img/almuerzo5.png'
+        img: './img/almuerzo5.png'
       }
     ],
     cenas: [
@@ -69,31 +69,31 @@ export default {
         id: 11,
         descripcion: 'Hamburguesa artesanal con carne de res, queso derretido, lechuga, tomate y papas fritas.',
         precio: 53,
-        img: '../img/cena1.png'
+        img: './img/cena1.png'
       },
       {
         id: 12,
         descripcion: 'Pasta Alfredo con pollo, preparada en salsa cremosa, acompañada de pan de ajo.',
         precio: 60,
-        img: '../img/cena2.png'
+        img: './img/cena2.png'
       },
       {
         id: 13,
         descripcion: 'Tacos de pollo sazonado, con cebolla, cilantro, salsa casera y limón.',
         precio: 35,
-        img: '../img/cena3.png'
+        img: './img/cena3.png'
       },
       {
         id: 14,
         descripcion: 'Sándwich caliente de jamón y queso, acompañado de papas fritas y aderezo.',
         precio: 29,
-        img: '../img/cena4.png'
+        img: './img/cena4.png'
       },
       {
         id: 15,
         descripcion: 'Quesadillas rellenas de queso derretido, acompañadas de frijoles y salsa.',
         precio: 32,
-        img: '../img/cena5.png'
+        img: './img/cena5.png'
       }
     ]
   }
